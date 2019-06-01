5a78c2452b67adc8039a2d0a4dd1e0a44dbcc3b6
Alan <alanfiscalcampos@gmail.com>
2019-06-01 06:38:27 -0500

    first commit

---
+++ webpack.config.js
@@ -0,0 +1,26 @@
+const path = require('path');
+const HtmlWebPackPlugin = require('html-webpack-plugin');
+
+const htmlWebPack = new HtmlWebPackPlugin({
+  template: './assets/index.template.html',
+  filename: 'index.html'
+});
+
+module.exports ={
+  entry: './assets/javascript/entry.js',
+  output: {
+    publicPath:'/',
+    path: path.join(__dirname,'..'),
+    filename: 'dist/javascript/bundle.js'
+  },
+  plugins:[htmlWebPack],
+  module:{
+    rules: [
+      {
+        test: /\.scss$/,
+        use: ['style-loader', 'css-loader','sass-loader']
+      }
+    ]
+  }
+
+}
