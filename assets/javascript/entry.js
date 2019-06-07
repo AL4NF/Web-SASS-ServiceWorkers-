3420e1d4c2f86e28bfda34309d78f517ae33feeb
Alan <alanfiscalcampos@gmail.com>
2019-06-07 14:19:22 -0500

    terminado

--- assets/javascript/entry.js
+++ assets/javascript/entry.js
@@ -1,2 +1,7 @@
 import '../css/app.scss';
-import './slider/sliderDOM'
+import './slider/sliderDOM';
+import './menu'
+
+if(navigator.serviceWorker){
+  navigator.serviceWorker.register('../sw.js');
+}
